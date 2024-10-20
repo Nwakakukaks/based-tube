fe402241907643e259c1a083db8cf3b2f9de8406
nwakakukaks <winnernwakaku123@gmail.com>
2024-10-20 13:58:14 +0100

    hidden footer on popups

--- frontend/components/general/Header.tsx
+++ frontend/components/general/Header.tsx
@@ -14,37 +14,47 @@ export function Header() {
     navigate("/dashboard");
   };
 
+ 
+  const isPaymentOrClaimOrAccess = 
+    location.pathname === "/payment" || 
+    location.pathname === "/claim" || 
+    location.pathname === "/access";
   return (
     <>
-      <div className="w-full bg-blue-50 p-1">
-        <p className="text-xs text-center text-black">
-          We appreciate you exploring our beta! Your feedback helps us grow and improve.
-        </p>
-      </div>
-
-      <div className="flex items-center justify-between px-4 py-2 max-w-screen-xl mx-auto w-full flex-wrap lg:px-20 mt-1">
-        <h3 onClick={handleHome} className="text-xl font-bold cursor-pointer">
-          SuperBase
-        </h3>
-
-        {location.pathname === "/" && (
-          <div className="flex items-center space-x-8 text-sm font-medium mt-1">
-            <p className="cursor-pointer">Company</p>
-            <p className="cursor-pointer">Product</p>
-            <p className="cursor-pointer">Contact us</p>
-          </div>
-        )}
-
-        <div className=" flex items-center space-x-4">
-          {location.pathname === "/" ? (
-            <Button onClick={handleLogin} className="bg-blue-600 rounded-full py-3 px-8 hover:bg-blue-300">
-              Login
-            </Button>
-          ) : (
-            <ConnectButton />
-          )}
+    {isPaymentOrClaimOrAccess ? (
+      <div></div>
+    ) : (
+      <>
+        <div className="w-full bg-red-50 p-1">
+          <p className="text-xs text-center text-black">
+            We appreciate you exploring our beta! Your feedback helps us grow and improve.
+          </p>
         </div>
-      </div>
-    </>
+        <div className="flex items-center justify-between px-4 py-2 max-w-screen-xl mx-auto w-full flex-wrap lg:px-20 mt-1">
+          <h3 onClick={handleHome} className="text-xl font-bold cursor-pointer">
+            Aptopus
+          </h3>
+
+          {location.pathname === "/" && (
+            <div className="flex items-center space-x-8 text-sm font-medium mt-1">
+              <p className="cursor-pointer">Company</p>
+              <p className="cursor-pointer">Product</p>
+              <p className="cursor-pointer">Contact us</p>
+            </div>
+          )}
+
+          <div className="flex items-center space-x-4">
+            {location.pathname === "/" ? (
+              <Button onClick={handleLogin} className="bg-blue-600 rounded-full py-3 px-8 hover:bg-blue-300">
+                Login
+              </Button>
+            ) : (
+              <ConnectButton />
+            )}
+          </div>
+        </div>
+      </>
+    )}
+  </>
   );
 }
--- frontend/components/general/L-Footer.tsx
+++ frontend/components/general/L-Footer.tsx
@@ -1,18 +1,29 @@
+import { useLocation } from "react-router-dom";
+
 export function LFooter() {
+  const location = useLocation();
+
+  // Check if the current path is one of the specified routes
+  const isFooterVisible = 
+    location.pathname !== "/payment" && 
+    location.pathname !== "/claim" && 
+    location.pathname !== "/access";
+
   return (
     <>
-      <div className="flex justify-between items-start my-6 px-20 ">
-        <div>
-          <p className="text-blue-500 font-semibold text-lg">SuperBase</p>
-          <p className="text-xs text-gray-500 w-48 lg:w-96">
-            Create unique links to recieve superchat in base tokens from your viewers during live streams.
-          </p>
+      {isFooterVisible && (
+        <div className="flex justify-between items-start my-6 px-20">
+          <div>
+            <p className="text-blue-500 font-semibold text-lg">SuperBase</p>
+            <p className="text-xs text-gray-500 w-48 lg:w-96">
+              Create unique links to receive Aptos tokens from your viewers during live streams.
+            </p>
+          </div>
+          <div className="flex space-x-3 items-center">
+            <img src="/icons/Twitter X.svg" className="bg-white rounded-sm w-6 h-6 mr-4" alt="Twitter" />
+          </div>
         </div>
-        <div className="flex space-x-3 items-center">
-          <img src="/icons/Twitter X.svg" className="bg-white rounded-sm w-6 h-6 mr-4" alt="Twitter" />
-        </div>
-      </div>
-
+      )}
       {/* Todo: add image here */}
     </>
   );
